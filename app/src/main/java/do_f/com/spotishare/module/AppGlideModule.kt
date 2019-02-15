ca86a8b2005da2502e1679fedd731db683dab521
do_f <florian.minhtan.do@gmail.com>
2019-02-14 22:20:00 -0500

    init

---
+++ app/src/main/java/do_f/com/spotishare/module/AppGlideModule.kt
@@ -0,0 +1,7 @@
+package do_f.com.spotishare.module
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+@GlideModule
+class AppGlideModule : AppGlideModule()
