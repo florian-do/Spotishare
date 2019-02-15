5183c378afc9906da09f83479ab28c8181737663
do_f <florian.minhtan.do@gmail.com>
2019-02-14 22:20:00 -0500

    init

---
+++ app/src/main/java/do_f/com/spotishare/api/service/Playlists.kt
@@ -0,0 +1,11 @@
+package do_f.com.spotishare.api.service
+
+import do_f.com.spotishare.api.model.MyPlaylistsResponse
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface Playlists {
+
+    @GET("me/playlists")
+    fun getMyPlaylists() : Call<MyPlaylistsResponse>
+}
