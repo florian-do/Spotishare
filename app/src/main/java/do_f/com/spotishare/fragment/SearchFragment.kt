1701ccbf359e37010f4fe566f96448dd7c119e93
do_f <florian.minhtan.do@gmail.com>
2019-02-15 15:23:36 -0500

    Implementation of the search feature without datasource add playlist add background change

--- app/src/main/java/do_f/com/spotishare/fragment/SearchFragment.kt
+++ app/src/main/java/do_f/com/spotishare/fragment/SearchFragment.kt
@@ -3,13 +3,21 @@ package do_f.com.spotishare.fragment
 import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
 import android.content.Context
+import android.databinding.DataBindingUtil
+import android.graphics.Bitmap
+import android.graphics.drawable.GradientDrawable
+import android.graphics.drawable.TransitionDrawable
 import android.net.Uri
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.support.annotation.LayoutRes
 import android.support.constraint.ConstraintSet
 import android.support.transition.ChangeBounds
 import android.support.transition.TransitionManager
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.util.Log
 import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
@@ -20,11 +28,23 @@ import android.view.animation.OvershootInterpolator
 import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
 import android.widget.TextView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.CustomViewTarget
+import com.bumptech.glide.request.target.SimpleTarget
+import com.bumptech.glide.request.target.Target
 import do_f.com.spotishare.R
+import do_f.com.spotishare.Utils
+import do_f.com.spotishare.adapters.SearchAdapter
+import do_f.com.spotishare.api.model.Item
+import do_f.com.spotishare.api.model.SearchResponse
+import do_f.com.spotishare.databinding.FragmentSearchBinding
 import do_f.com.spotishare.view.MyEditText
 import do_f.com.spotishare.viewmodel.SearchViewModel
 import kotlinx.android.synthetic.main.fragment_search.*
-import retrofit2.Callback
+import kotlinx.android.synthetic.main.fragment_slave.*
 
 class SearchFragment : Fragment() {
 
@@ -35,7 +55,12 @@ class SearchFragment : Fragment() {
     private val constraint1 = ConstraintSet()
     private val constraint2 = ConstraintSet()
     private var set = false
+    private var items : MutableList<Item> = mutableListOf()
+
     private lateinit var searchViewModel : SearchViewModel
+    private lateinit var adapter : SearchAdapter
+    private lateinit var  binding : FragmentSearchBinding
+    private val mHandler = Handler(Looper.getMainLooper())
 
     companion object {
         private const val ARG_PARAM1 = "param1"
@@ -54,9 +79,11 @@ class SearchFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?): View? {
 
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_search, container, false)
         searchViewModel = ViewModelProviders.of(this).get(SearchViewModel::class.java)
+        adapter = SearchAdapter(Glide.with(this.context!!))
 
-        return inflater.inflate(R.layout.fragment_search, container, false)
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -68,20 +95,23 @@ class SearchFragment : Fragment() {
         doAnimation()
 
         search_content.setOnClickListener {
-            doAnimation()
+            if (!set && items.isEmpty())
+                doAnimation()
         }
 
         search_arrow.setOnClickListener {
             fragmentManager?.popBackStack()
         }
 
+        rvFeed.layoutManager = LinearLayoutManager(context)
+        rvFeed.setHasFixedSize(true)
+        rvFeed.isNestedScrollingEnabled = false
+        rvFeed.adapter = adapter
 
         search_field.setOnEditorActionListener(object : TextView.OnEditorActionListener{
             override fun onEditorAction(p0: TextView?, action: Int, p2: KeyEvent?): Boolean {
                 if (action == EditorInfo.IME_ACTION_DONE) {
-                    searchViewModel.search(search_field.text.toString()).observe(this@SearchFragment, Observer {
-
-                    })
+                    callApi()
                     return true
                 }
                 return false
@@ -90,11 +120,80 @@ class SearchFragment : Fragment() {
 
         search_field.setKeyBackListener(object : MyEditText.OnKeyImeListener {
             override fun onKeyIme(keyCode: Int, event: KeyEvent?) {
-                if (set && event?.action == KeyEvent.ACTION_DOWN) {
+                if (set && event?.action == KeyEvent.ACTION_DOWN && search_field.text.isEmpty()) {
                     doAnimation()
                 }
             }
         })
+    }
+
+    private fun callApi() {
+        searchViewModel.search(search_field.text.toString())
+            .observe(this, Observer {
+                if (it != null) {
+                    items.clear()
+                    buildSearchQuery(it)
+                    Log.d(TAG, "observe ${items.size}")
+                    binding.isContent = items.isNotEmpty()
+                    if (items.isNotEmpty())
+                        hideKeyboard()
+                    adapter.refreshData(items)
+                }
+        })
+    }
+
+    private fun buildSearchQuery(data : SearchResponse) {
+        if (data.artists.total > 0) {
+            items.add(data.artists.items[0])
+            changeBackground(data.artists.items[0].images[0].url)
+        }
+
+        data.playlists.let {
+            if (it.items.isNotEmpty()) {
+                if (it.items.size > 1)
+                    for (i in 0..1) { items.add(it.items[i]) }
+                else
+                    items.add(it.items[0])
+            }
+        }
+
+        if (data.tracks.total > 0)
+            items.addAll(data.tracks.items)
+    }
+
+    private fun changeBackground(url : String) {
+        Glide.with(this)
+            .asBitmap()
+            .load(url)
+            .listener(object : RequestListener<Bitmap> {
+                override fun onLoadFailed(e: GlideException?, model: Any?,
+                    target: Target<Bitmap>?, isFirstResource: Boolean): Boolean {
+                    return false
+                }
+
+                override fun onResourceReady(resource: Bitmap?, model: Any?,
+                    target: Target<Bitmap>?, dataSource: DataSource?, isFirstResource: Boolean): Boolean {
+
+                    setBackground(resource!!)
+                    return false
+                }
+
+            }).submit()
+    }
+
+    private fun setBackground(bitmap: Bitmap) {
+        val domColor = Utils.manipulateColor(Utils.getDominantColor(bitmap), 0.6F)
+        val backgroundColor = resources.getColor(R.color.background)
+        val colors = intArrayOf(domColor, backgroundColor, backgroundColor)
+
+        val gd = GradientDrawable(
+            GradientDrawable.Orientation.TL_BR, colors
+        )
+
+        val transition = TransitionDrawable(arrayOf(rvFeed.background, gd))
+        transition.isCrossFadeEnabled = true
+        rvFeed.background = transition
+        transition.startTransition(1000)
     }
 
     private fun doAnimation() {
@@ -131,10 +230,16 @@ class SearchFragment : Fragment() {
         TransitionManager.beginDelayedTransition(root, transition)
     }
 
+    private fun hideKeyboard() {
+        activity?.let {
+            val imm : InputMethodManager = it.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(view?.windowToken, 0)
+        }
+    }
+
     override fun onStop() {
         super.onStop()
-        val imm : InputMethodManager = activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.hideSoftInputFromWindow(view?.windowToken, 0)
+        hideKeyboard()
     }
 
     override fun onAttach(context: Context) {
