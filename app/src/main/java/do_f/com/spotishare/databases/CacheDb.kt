acfc1c8d9e2a45f59c13fc6841b809ecca7dff5b
do_f <florian.minhtan.do@gmail.com>
2019-02-21 02:39:38 -0500

    Implement room for playlistView in SongsFragment

--- app/src/main/java/do_f/com/spotishare/databases/CacheDb.kt
+++ app/src/main/java/do_f/com/spotishare/databases/CacheDb.kt
@@ -3,15 +3,17 @@ package do_f.com.spotishare.databases
 import android.arch.persistence.room.Database
 import android.arch.persistence.room.RoomDatabase
 import android.arch.persistence.room.TypeConverters
+import do_f.com.spotishare.databases.converters.RowTypeConverter
+import do_f.com.spotishare.databases.entities.Playlist
 import do_f.com.spotishare.databases.entities.Playlists
 
 @Database(
-    entities = [Playlists::class],
-    version = 4,
+    entities = [Playlists::class, Playlist::class],
+    version = 5,
     exportSchema = false
 )
 
-@TypeConverters(ImageTypeConverter::class)
+@TypeConverters(ImageTypeConverter::class, RowTypeConverter::class)
 abstract class CacheDb : RoomDatabase() {
     companion object {
         val DB_NAME = "cache.db"
