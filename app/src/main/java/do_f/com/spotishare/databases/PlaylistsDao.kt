b9ac35cdbb83ecffed9cff212dfb16226b548d8e
do_f <florian.minhtan.do@gmail.com>
2019-02-21 02:39:38 -0500

    Implement room for playlistView in SongsFragment

--- app/src/main/java/do_f/com/spotishare/databases/PlaylistsDao.kt
+++ app/src/main/java/do_f/com/spotishare/databases/PlaylistsDao.kt
@@ -5,6 +5,7 @@ import android.arch.persistence.room.Dao
 import android.arch.persistence.room.Insert
 import android.arch.persistence.room.OnConflictStrategy
 import android.arch.persistence.room.Query
+import do_f.com.spotishare.databases.entities.Playlist
 import do_f.com.spotishare.databases.entities.Playlists
 
 @Dao
@@ -17,4 +18,13 @@ interface PlaylistsDao {
 
     @Query("SELECT COUNT(*) FROM playlists")
     fun count(): Int
+
+    @Query("SELECT * FROM playlist WHERE id=:id")
+    fun getPlaylistById(id: String) : LiveData<Playlist>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun instertById(item : Playlist)
+
+    @Query("SELECT COUNT(*) FROM playlist WHERE id=:id")
+    fun isPlaylistInDb(id: String) : Int
 }
---
+++ app/src/main/java/do_f/com/spotishare/databases/entities/Type.kt
@@ -0,0 +1,8 @@
+package do_f.com.spotishare.databases.entities
+
+enum class Type(val type : String) {
+    PLAYLIST("playlist"),
+    SONG("song"),
+    ARTIST("artist")
+
+}
