4162a9dee86ada503f4e629478b973ad1326075b
do_f <florian.minhtan.do@gmail.com>
2019-02-22 03:40:44 -0500

    Init Firebase Database

--- app/src/main/java/do_f/com/spotishare/App.kt
+++ app/src/main/java/do_f/com/spotishare/App.kt
@@ -2,6 +2,8 @@ package do_f.com.spotishare
 
 import android.app.Application
 import android.arch.persistence.room.Room
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import do_f.com.spotishare.api.RefreshStrategy
 import do_f.com.spotishare.api.SpotifyClient
 import do_f.com.spotishare.databases.CacheDb
@@ -10,6 +12,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class App : Application() {
     companion object {
+        lateinit var firebaseDb : DatabaseReference
         lateinit var mRefreshStrategy : RefreshStrategy
         lateinit var cacheDb : CacheDb
         var mSpotifyClient = SpotifyClient()
@@ -22,6 +25,7 @@ class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        firebaseDb = FirebaseDatabase.getInstance().reference
         mRefreshStrategy = RefreshStrategy(applicationContext)
         cacheDb = Room.databaseBuilder(applicationContext, CacheDb::class.java, CacheDb.DB_NAME)
             .fallbackToDestructiveMigration().build()
