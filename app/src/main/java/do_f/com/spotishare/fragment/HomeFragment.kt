1306e5799315f695a9fbcee59ce747b44e77408d
do_f <florian.minhtan.do@gmail.com>
2019-02-22 23:29:22 -0500

    Implement pin view

--- app/src/main/java/do_f/com/spotishare/fragment/HomeFragment.kt
+++ app/src/main/java/do_f/com/spotishare/fragment/HomeFragment.kt
@@ -1,86 +1,86 @@
 package do_f.com.spotishare.fragment
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.databinding.DataBindingUtil
 import android.graphics.Bitmap
-import android.net.Uri
 import android.os.Bundle
-import android.support.v4.app.Fragment
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.Navigation
+import androidx.navigation.NavOptions
+import androidx.navigation.fragment.NavHostFragment.findNavController
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
-import com.spotify.android.appremote.api.SpotifyAppRemote
-import com.spotify.sdk.android.authentication.AuthenticationClient
-import com.spotify.sdk.android.authentication.AuthenticationRequest
-import com.spotify.sdk.android.authentication.AuthenticationResponse
+
 import do_f.com.spotishare.App
-import do_f.com.spotishare.MainActivity
 import do_f.com.spotishare.R
 import do_f.com.spotishare.Utils
 import do_f.com.spotishare.base.BFragment
 import do_f.com.spotishare.databinding.FragmentHomeBinding
 import do_f.com.spotishare.dialogfragment.RoomFragment
-import do_f.com.spotishare.model.Queue
+
 import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : BFragment() {
 
     private var TAG = "HomeFragment"
     private var mListener: OnFragmentInteractionListener? = null
+    private val REQUEST_CODE = 1337
+
     private lateinit var mBinding : FragmentHomeBinding
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        mBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
+        mBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_home,
+            container, false)
         return mBinding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         master.setOnClickListener {
-            MainActivity.roomCode = Utils.generateRoomNumber()
-            App.firebaseDb.child(MainActivity.roomCode).push().setValue("init")
-            Navigation.findNavController(it).navigate(R.id.discoverFragment)
-
-//            val arg = Bundle()
-//            arg.putString(MasterFragment.ARG_PARAM1, "HEY HO HEY")
-//            Navigation.findNavController(it).navigate(R.id.masterFragment, arg)
-//            App.firebaseDb.child(Utils.generateRoomNumber()).setValue("create")
-//            App.firebaseDb.child(Utils.generateRoomNumber()).push().setValue("push create")
+            App.roomCode = Utils.generateRoomNumber()
+            App.firebaseDb.child(App.roomCode).push().setValue("init")
+            initSession()
         }
 
         slave.setOnClickListener {
             val f : RoomFragment = RoomFragment.newInstance()
+            f.setTargetFragment(this, REQUEST_CODE)
             f.show(fragmentManager, null)
-//            Navigation.findNavController(it).navigate(R.id.slaveFragment)
         }
 
-        cover_placeholder.setOnClickListener {
-
-            App.firebaseDb.child(Utils.generateRoomNumber()).addListenerForSingleValueEvent(object : ValueEventListener {
-                override fun onCancelled(p0: DatabaseError) {
-                    Log.e(TAG, "error: ", p0.toException())
-                }
-
+        cover.setOnClickListener {
+            App.firebaseDb.child("NZ29").addListenerForSingleValueEvent(object : ValueEventListener {
                 override fun onDataChange(p0: DataSnapshot) {
-
+                    Log.d(TAG, "${p0.children.first().key}")
+                    App.firebaseDb.child("NZ29").child(p0.children.first().key!!).removeValue()
+                    p0.children.forEach {
+                        Log.d(TAG, "${it.key}")
+                    }
                 }
 
+                override fun onCancelled(p0: DatabaseError) { }
             })
-
-            App.firebaseDb
-                .child(MainActivity.queueSize.toString())
-                .setValue(Queue("spotify:song:1Yfe3NJlioHys7jwHdfVm", "temp", "Lomepal", true))
         }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            initSession()
+        }
+    }
+
+    private fun initSession() {
+        mListener?.initQueue()
+        findNavController(this).navigate(R.id.discoverFragment,
+            null,
+            NavOptions.Builder().setPopUpTo(R.id.homeFragment, true).build())
     }
 
     fun setImage(bitmap: Bitmap) {
@@ -91,16 +91,12 @@ class HomeFragment : BFragment() {
 
     }
 
-    /**
-     * Communication between activity and fragment
-     */
-
     override fun onAttach(context: Context) {
         super.onAttach(context)
         if (context is OnFragmentInteractionListener) {
             mListener = context
         } else {
-//            throw RuntimeException(context.toString() + " must implement OnFragmentInteractionListener")
+            throw RuntimeException(context.toString() + " must implement OnFragmentInteractionListener")
         }
     }
 
@@ -110,6 +106,6 @@ class HomeFragment : BFragment() {
     }
 
     interface OnFragmentInteractionListener {
-        fun onFragmentInteraction(uri: Uri)
+        fun initQueue()
     }
 }
