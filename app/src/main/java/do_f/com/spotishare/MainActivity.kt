6684f47f629f2313ea08b5cd5fc191c53e4b845c
do_f <florian.minhtan.do@gmail.com>
2019-02-27 02:12:57 -0500

    remove API KEY

--- app/src/main/java/do_f/com/spotishare/MainActivity.kt
+++ app/src/main/java/do_f/com/spotishare/MainActivity.kt
@@ -43,9 +43,9 @@ class MainActivity : AppCompatActivity(),
 
     companion object {
         private val TAG = "MainActivity";
-        val CLIENT_ID = "***REMOVED***";
+        val CLIENT_ID = BuildConfig.SPOTIFY_API_KEY
         val REQUEST_CODE = 1337
-        val REDIRECT_URI = "***REMOVED***"
+        val REDIRECT_URI = BuildConfig.SPOTIFY_CALLBACK
         val MESSAGING_SCOPE = "https://www.googleapis.com/auth/firebase.messaging"
         val FCM_INTENT_FILTER = "fcm_service_intent_filter"
         var isSpotifyInstalled : Boolean = false
